4b62039544d1c8172888cc7fde1ec4fdca6bd141
Morris Muchiri <muchirimorrizon14@gmail.com>
2026-03-12 01:09:05 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Conditionally set the base path based on Vercel environment
-  base: process.env.VERCEL ? "/" : "/morris.portfolio/",
+  // Conditionally set the base path based on Vercel or Netlify environment
+  base: (process.env.VERCEL || process.env.NETLIFY) ? "/" : "/morris.portfolio/",
 })
